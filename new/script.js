bf25543f28d52216bbc5aa669504162d11e5d57a
ordinaldev <121743907+ordinaldev@users.noreply.github.com>
2023-05-16 21:53:54 +0700

    Add files via upload

---
+++ new/script.js
@@ -0,0 +1,28 @@
+
+// function muncul() {
+
+//     document.write(`<video width="450" height="auto" controls >
+//         <source src="./aset/16.Smooth Camera Follow in Unity - Tutorial.mp4" type="video/mp4">
+//         browser mu ga support video
+//         </video> \n\n  <a href="./index2.html"> close video</a>`)
+// }
+// function tutup() {
+//     document.write('<button onclick="muncul()"> see video</button>')
+//     document.write('<button onclick="muncul2()"> see music</button>')
+//     document.write('<button onclick="muncul3()"> see Youtube</button>')
+//     document.write('<button onclick="muncul4()"> see My web</button>')
+// }
+// function muncul2() {
+//     document.write(`<audio controls>
+//     <source src="./aset/Suara Hujan Dan Petir  Untuk Terapi Tidur Dan Relax.mp3" type="audio/mpeg">
+//     Your browser does not support the audio element.
+// </audio> \n\n <a href="./index2.html"> close Music</a>`)
+// }
+// function muncul3() {
+//     document.write(`<iframe width="450" height="300" src="https://www.youtube.com/embed/D1FOPvcBIp8">
+// </iframe> \n\n <a href="./index2.html"> close Youtube</a>`)
+// }
+// function muncul4() {
+//     document.write(`<embed width="450" height="300" src="https://ordinalblog.netlify.app/">
+// \n\n <a href="./index2.html"> close web</a>`)
+// }
